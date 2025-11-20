f906af1bf99759e97772abc23d08efcf1969604d
Omkar Pandey <omkarpandey1787@gmail.com>
2025-11-20 22:06:53 +0530

    eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-// Disable TS rules + allow JS build on Railway
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+// eslint.config.mjs
+export default [
+  {
+    ignores: ["**/*.ts", "**/*.tsx"],
+  },
   {
     rules: {
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
       "no-unused-vars": "off",
-      "no-undef": "off",
+      "no-undef": "off"
     }
   }
 ];
-
-export default eslintConfig;
